1c24b96b7223f8628bd8e193b823d5b2b50a79d1
Foxx Suzuki <begreflex@gmail.com>
2022-06-03 06:14:20 -0400

    Initial commit

---
+++ YouTubeDownloadTry2/Main.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace YouTubeDownload
+{
+    class YoutubeDL
+    {
+
+        static void Main(string[] args)
+        {
+            var _text = Clipboard.GetText();
+            Console.WriteLine(_text);
+            Console.WriteLine("Calling youtube-dl...");
+            if (_text.Contains("http://") || _text.Contains("https://"))
+                try
+                {
+                    Process.Start("youtube-dl", "-f 140 --output %(title)s.%(ext)s " + _text);
+                }
+                catch (Exception ex) { Console.WriteLine("Could Not Grab Title Name will be normal\n {0}", ex.Message); Process.Start("youtube-dl", "-f 140 " + _text); }
+            else
+            {
+                Console.WriteLine("Text did not Contain a youtube link");
+                Console.ReadLine();
+            }
+        }
+    }
+}
